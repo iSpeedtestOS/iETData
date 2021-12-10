bdc0b85c08594d09d15b266bf166cbd82f8c6a18
iSpeedtestOS <60826079+iSpeedtestOS@users.noreply.github.com>
2021-12-10 22:20:47 +0100

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,6 +1,6 @@
 var events = [
 	{
-		"name": "iEventTimer",
+		"name": "iEventTimer beta",
 		"evDate": "Oct 25, 2021 10:00:00 GMT-07:00",
 		"notificationID": "macos12release",
 		"tagline": "High powered meets Hi everyone.",
