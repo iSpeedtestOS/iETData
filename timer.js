0f438f8d111c1961db0ada5bb900c0d40dadf96b
iSpeedtestOS <60826079+iSpeedtestOS@users.noreply.github.com>
2022-03-04 00:13:09 +0100

    Update timer.js

--- timer.js
+++ timer.js
@@ -7,7 +7,6 @@ var events = [
 		"kind": "event"
 	}
 ];
-//PSE
 
 //var eventDatee = new Date("Oct 5, 2021 6:00:00 GMT-07:00").getTime();
 var eventDate = new Date("Mar 08, 2022 10:00:00 GMT-08:00").getTime();
