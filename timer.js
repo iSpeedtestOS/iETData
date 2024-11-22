7307dd3ec28d516cca319f16e017772d8b9b8ddd
iSpeedtestOS <60826079+iSpeedtestOS@users.noreply.github.com>
2024-11-22 14:52:16 +0100

    Update timer.js

--- timer.js
+++ timer.js
@@ -13,7 +13,7 @@ var events = [
 		"tagline": "Yours. Truly.",
 		"kind": "release"
 	}
-];//04010105
+];
 
 //var eventDatee = new Date("Oct 5, 2021 6:00:00 GMT-07:00").getTime();
 var eventDate = new Date("Jun 10, 2024 10:00:00 GMT-07:00").getTime();
