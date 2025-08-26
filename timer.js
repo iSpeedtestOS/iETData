9f268997b8a80e0a7eaed45625996542fcbd7d75
iSpeedtestOS <60826079+iSpeedtestOS@users.noreply.github.com>
2025-08-26 18:15:12 +0200

    Update timer.js

--- timer.js
+++ timer.js
@@ -40,7 +40,7 @@ var eventNameBox = document.getElementById('eventname');
 
 
 
-  var jsonurl = "https://events-delivery.apple.com/0705jnbbtdldymmnmhvadmcwzjqrrfdjrzlp/js_files/event/url.json";
+  //var jsonurl = "https://events-delivery.apple.com/0705jnbbtdldymmnmhvadmcwzjqrrfdjrzlp/js_files/event/url.json";
 
 
 
